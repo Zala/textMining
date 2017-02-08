634f9466b106a06a618ab60304ebcecc56ff08dd
MihaTorkar <miha.torkar0@gmail.com>
2017-02-08 17:41:25 +0100

    Added counts for time series

--- extractFeatures.js
+++ extractFeatures.js
@@ -44,14 +44,89 @@ let news = base.store('news').allRecords;
 
 let ftr = new qm.FeatureSpace(base, 
     { type: "multinomial", source: "news", field: "concepts", 
-        values: ["Gay", "Abortion"] }       
+        values: ["Gay", "Abortion"] }       // add new concepts if needed
 );
 ftr.addFeatureExtractor(
-    { type: "multinomial", source: "news", field: "date", datetime: true }
+    { type: "multinomial", source: "news", field: "date",datetime: true }
 );
 
 ftr.updateRecords(news);
 console.timeEnd("feature_extr");
+
+// Create store for Time Series 
+let tsSchema = {
+    name: "ts",
+    fields: [
+        { name: "Time", type: "datetime" },
+        { name: "gayCount", type: "float" },
+        { name: "abortionCount", type: "float" } //add new concepts
+    ]
+}
+
+let tsStore = base.createStore(tsSchema);
+
+console.log("tsStore created");
+
+// Initialise counts
+var gaycount = 0;
+var abortioncount = 0;
+
+for (var i = 0; i < newsStore.allRecords.length; i++) {
+    var rec = newsStore[i];
+    let recPrev = newsStore[0];
+    if (i > 0)
+        recPrev = newsStore[i - 1];
+    
+    var vec = ftr.extractSparseVector(rec);
+    var valVec = vec.valVec();
+    var idxVec = vec.idxVec();
+   
+  
+    // increase counts
+    if (rec.date.setHours(0, 0, 0) == recPrev.date.setHours(0, 0, 0)) { // condition that we are in the same day
+        
+        for (var j = 0; j < idxVec.length; j++) { // idxVec.length - 4
+            if( ftr.getFeature(idxVec[j]) == 'Gay')
+                gaycount += 1
+            if( ftr.getFeature(idxVec[j]) == 'Abortion')
+                abortioncount += 1;
+        }
+    }
+    else { // date changed so we push the value into tsStore
+
+        tsStore.push({
+            Time: new Date(recPrev.date).toISOString(),
+            gayCount: gaycount,
+            abortionCount: abortioncount
+        });
+        // reset the counts
+        gaycount = 0;
+        abortioncount = 0;
+
+        //  report the results
+        let newRec = tsStore.last;
+        console.log(
+            "Date:",
+            newRec.Time.toISOString(),
+            "Gay:",
+            newRec.gayCount,
+            "Abortion: ",
+            newRec.abortionCount
+        );
+
+        // Check the first element of new date
+        for (var j = 0; j < idxVec.length; j++) { // idxVec.length - 4
+            if (ftr.getFeature(idxVec[j]) == 'Gay')
+                gaycount += 1
+            if (ftr.getFeature(idxVec[j]) == 'Abortion')
+                abortioncount += 1;
+        }
+    }
+}
+
+
+console.log("tsStore filled");
+
 
 
 //let M = ftr.extractSparseMatrix(news);
@@ -61,14 +136,14 @@ console.timeEnd("feature_extr");
 //ftr.getFeature(0);
 //first.at(0); //value at 0 ind
 
-var rec = newsStore[1];
-var vec = ftr.extractSparseVector(rec);
-var idxVec = vec.idxVec();
-var valVec = vec.valVec();
-for (var i = 0; i < idxVec.length; i++) {
-    console.log(ftr.getFeature(idxVec[i]) + " " + valVec[i]);
-}
-
+//var rec = newsStore[1];//3393
+//var vec = ftr.extractSparseVector(rec);
+//var idxVec = vec.idxVec();
+//var valVec = vec.valVec();
+//console.log(idxVec.length)
+//for (var i = 0; i < idxVec.length; i++) {
+//    console.log(ftr.getFeature(idxVec[i]) + " " + valVec[i]);
+//}
 
 debugger
 
